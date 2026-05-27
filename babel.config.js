c13556f6881074bbac4fe591218ee73431977fc5
paladiini <paladiini@Vitor-Laptop.localdomain>
2026-05-26 21:40:16 -0300

    fix: move nativewind/babel to presets in babel.config.js to resolve plugin validation error

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   const isTest = process.env.NODE_ENV === 'test';
   return {
-    presets: ['babel-preset-expo'],
-    plugins: isTest ? [] : ['nativewind/babel', 'react-native-reanimated/plugin'],
+    presets: isTest ? ['babel-preset-expo'] : ['babel-preset-expo', 'nativewind/babel'],
+    plugins: isTest ? [] : ['react-native-reanimated/plugin'],
   };
 };
